eaf003a5cccebe40d7f8e188a4da2864a08b2c49
Victor Garcia <victorgf2011@gmail.com>
2023-09-19 18:24:47 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow};
+
+const APP_ID: &str = "eu.tortitas.runst";
+
+fn main() -> glib::ExitCode {
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    // Create a window and set the title
+    let window = ApplicationWindow::builder().application(app).build();
+
+    // Present window
+    window.present();
+}
